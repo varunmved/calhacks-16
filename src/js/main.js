5792f5fc148a3946f8d301beea9d50e53eddea5d
davidjudilla <davidfjudilla@gmail.com>
2016-11-12 17:20:10 -0800

    added file upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 window.onload = function(){
-	console.log('hello')
 	var variables = []
 
 	const varInput = document.getElementById("var-input")
+	const send = document.getElementById("send")
 
 	function updateVarList(variables) {
 		const varList = document.getElementById("var-list")
@@ -21,8 +21,31 @@ window.onload = function(){
 		}
 	})
 
-	$('#var-list').addEventListener('click', 'li', function(e) {
-		
+	send.addEventListener('click', function(e) {
+		const fileInput = document.getElementById("csv")
+		var file = fileInput.files[0]
+		var form = new FormData()
+		form.append('file', file)
+		console.log(form)
+
+		/*$.ajax({
+			url : "/upload",
+	        type: "POST",
+	        cache: false,
+	        contentType: false,
+	        processData: false,
+	        data : form,
+	        success: function(response){
+	            console.log('success')
+	            console.log(response)
+	        }
+		})*/
+	})
+
+
+
+	$('#var-list').on('click', 'li', function(e) {
+
 	})
 
 }

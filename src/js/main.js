a11559f67c031db5ef82810b1a3f1ec6a51acd7e
joeylaguna <elagunaj@gmail.com>
2016-11-13 00:38:37 -0800

    Added graph props to index.html & js to make them work.
    Need to fix css

--- src/js/main.js
+++ src/js/main.js
@@ -49,5 +49,39 @@ window.onload = function(){
 
 }
 
+//Sliders
+var el, newPoint, newPlace, offset;
+ 
+// Select all range inputs, watch for change
+$("input[type='range']").change(function() {
+
+ // Cache this for efficiency
+ el = $(this);
+ 
+ // Measure width of range input
+ width = el.width();
+ 
+ // Figure out placement percentage between left and right of input
+ newPoint = (el.val() - el.attr("min")) / (el.attr("max") - el.attr("min"));
+  
+  offset = -1;
+
+ // Prevent bubble from going beyond left or right (unsupported browsers)
+ if (newPoint < 0) { newPlace = 0; }
+ else if (newPoint > 1) { newPlace = width; }
+ else { newPlace = width * newPoint + offset; offset -= newPoint; }
+ 
+ // Move bubble
+ el
+   .next("output")
+   .css({
+     left: newPlace,
+     marginLeft: offset + "%"
+   })
+     .text(el.val());
+ })
+ // Fake a change to position bubble at page load
+ .trigger('change');
+
 
 
